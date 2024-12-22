3614a30fff4ef3f57683e4214ec426f84094956c
Indosiasat <maarifagus@gmail.com>
2024-12-22 08:10:25 +0700

    Update trojan.js
    Signed-off-by: Indosiasat <maarifagus@gmail.com>

--- trojan workers/trojan.js
+++ trojan workers/trojan.js
@@ -2,7 +2,7 @@
 import { connect } from "cloudflare:sockets";
  
 let Pswd = "trojan";
-const proxyIPs = ["herza.id"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
+const proxyIPs = ["103.168.146.169":20132]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
 let cn_hostnames = [''];
 let CDNIP = 'www.visa.com.sg'
 // http_ip
